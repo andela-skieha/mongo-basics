f0edf7be32818f46c078ad30b3bb43ab25a38840
Njeri Kieha <sylvia.kieha@andela.com>
2016-08-05 10:12:20 +0300

    refactor

--- index.js
+++ index.js
@@ -11,38 +11,37 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'CONNECTION ERROR:'));
 db.once('open', function() {
 	console.log('Connection Successful');
-});
+	// reference to schema
+	var kittySchema = mongoose.Schema({
+		name: String
+	});
 
-// reference to schema
-var kittySchema = mongoose.Schema({
-	name: String
-});
-
-// adding methods to kitten document
-kittySchema.methods.speak = function() {
-	var greeting = this.name
+	// adding methods to kitten document
+	kittySchema.methods.speak = function() {
+		var greeting = this.name
 		? 'Meow name is ' + this.name
 		: 'I don\'t have a name';
-	console.log(greeting);
-};
+		console.log(greeting);
+	};
 
-// Model definition
-var Kitten = mongoose.model('Kitten', kittySchema);
+	// Model definition
+	var Kitten = mongoose.model('Kitten', kittySchema);
 
-// kitten document
-var silence = new Kitten({ name: 'Silence'});
-// saving kitten document to mongodb
-silence.save(function(err, silence) {
-	if (err) {
-		return console.error(err);
-	}
-	silence.speak();
-});
+	// kitten document
+	var silence = new Kitten({ name: 'Silence'});
+	// saving kitten document to mongodb
+	silence.save(function(err, silence) {
+		if (err) {
+			return console.error(err);
+		}
+		silence.speak();
+	});
 
-// displaying all kittens in the db
-Kitten.find(function(err, kittens) {
-	if (err) {
-		return console.error(err);
-	}
-	console.log(kittens);
+	// displaying all kittens in the db
+	Kitten.find(function(err, kittens) {
+		if (err) {
+			return console.error(err);
+		}
+		console.log(kittens);
+	});
 });
